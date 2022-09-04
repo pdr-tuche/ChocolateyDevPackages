efa3a2b8d2e6fc2ad40ed379f0a32c9f01641558
pdr-tuche <contato.nevespedro@gmail.com>
2022-09-04 19:59:05 -0300

    adicionado pacotes para instalacao do reactnative, python e java

--- installCLI.ps1
+++ installCLI.ps1
@@ -7,15 +7,18 @@ switch ( $option )
         choco install -y openjdk11
         winget install --id Google.AndroidStudio
         Start-Sleep -Seconds 3
-        Write-Output instalacao finalizada
+        Write-Output "instalacao REACT NATIVE finalizada"
     }
     2 { 
         winget install --id Python.Python.3.8
         winget install --id Anaconda.Anaconda3
         winget install --id Microsoft.VisualStudioCode
+        Write-Output "instalacao Python finalizada"
     }
     3 { 
-        #colocar aqui depedencias do java
+        choco install -y openjdk11
+        winget install --id JetBrains.IntelliJIDEA.Community
+        Write-Output "instalacao Java finalizada"
     }
 }
 
