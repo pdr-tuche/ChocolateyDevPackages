c3cf088e4de3c9dc7185d08afa63ef87f0058717
pdr-tuche <contato.nevespedro@gmail.com>
2022-09-04 23:09:26 -0300

    adicionado readme

--- installCLI.ps1
+++ installCLI.ps1
@@ -16,6 +16,7 @@ switch ( $option )
         Write-Output "instalacao Python finalizada"
     }
     3 { 
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         choco install -y openjdk11
         winget install --id JetBrains.IntelliJIDEA.Community
         Write-Output "instalacao Java finalizada"
