0ec32c400e1e6c246ece65e92880d33e774bc721
pdr-tuche <contato.nevespedro@gmail.com>
2022-09-08 22:49:21 -0300

    criado header

--- installCLI.ps1
+++ installCLI.ps1
@@ -1,14 +1,23 @@
+Write-Output "##########################################"
+Write-Output "#               Welcome to               #"
+Write-Output "#          InstaladorDevWindows          #"
+Write-Output "#      Follow me on GitHub: @pdr0nvs     #"
+Write-Output "##########################################"
+Start-Sleep -Seconds 3
+
 do{
+    Write-Output ""
+    Write-Output ""
+    Write-Output "Selecione o que ira desenvolver (Select what you will develop):"
     $option = Read-Host "1 - React-Native    2-Python    3-java"
-    switch ( $option )
-    {
+    switch ( $option ){
         1 {
             Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
             choco install -y nodejs-lts
             choco install -y openjdk11
             winget install --id Google.AndroidStudio
-            Start-Sleep -Seconds 3
             Write-Output "instalacao REACT NATIVE finalizada"
+            
         }
         2 { 
             winget install --id Python.Python.3.8
@@ -19,13 +28,13 @@ do{
         3 { 
             Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
             choco install -y openjdk11
-            winget install --id JetBrains.IntelliJIDEA.Community
+         #   winget install --id JetBrains.IntelliJIDEA.Community
             Write-Output "instalacao Java finalizada"
         }
     }
     $flag = Read-Host "Deseja fazer mais uma instalacao ? [S] / [N]"
 }while($flag -like "S" -or $flag -like "s")
 
-Write-Output "Follow me on GitHub: @pdr0nvs"
+
 Write-Output "See ya!! ^^"
 
