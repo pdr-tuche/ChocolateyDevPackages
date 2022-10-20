df3e0fcfc4339d9281229bdf6b9ef58c816bfb9b
Pedro Neves <contato.nevespedro@gmail.com>
2022-10-20 16:23:44 -0300

    tirado comandos winget. Fixed #1

--- installCLI.ps1
+++ installCLI.ps1
@@ -3,38 +3,35 @@ Write-Output "#               Welcome to               #"
 Write-Output "#          InstaladorDevWindows          #"
 Write-Output "#     Follow me on GitHub: @pdr-tuche    #"
 Write-Output "##########################################"
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 2
 
 do{
     Write-Output ""
     Write-Output ""
     Write-Output "Selecione o que ira desenvolver :"
-    $option = Read-Host "1 - React-Native    2-Python    3-java"
+    $option = Read-Host "1- React-Native    2- Python    3- java    4- VSCode"
+
     switch ( $option ){
         1 {
-            Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
             choco install -y nodejs-lts
             choco install -y openjdk11
-            winget install --id Google.AndroidStudio
+            choco install -y androidstudio
             Write-Output "instalacao REACT NATIVE finalizada"
-            
         }
-        2 { 
-            winget install --id Python.Python.3.8
-            winget install --id Anaconda.Anaconda3
-            winget install --id Microsoft.VisualStudioCode
+        2 {
+            choco install -y python
             Write-Output "instalacao Python finalizada"
         }
         3 { 
-            Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
             choco install -y openjdk11
-            winget install --id JetBrains.IntelliJIDEA.Community
             Write-Output "instalacao Java finalizada"
+        }
+        4 {
+            choco install -y vscode
+            Write-Output "instalacao Visual Studio Code finalizada"
         }
     }
     $flag = Read-Host "Deseja fazer mais uma instalacao ? [S] / [N]"
 }while($flag -like "S" -or $flag -like "s")
 
-
 Write-Output "See ya!! ^^"
-
